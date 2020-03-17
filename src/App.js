df62721e7c674e0515b724d75e2914adb4e5f6c2
baixian <1325165625@qq.com>
2020-03-17 19:32:45 +0800

    videolist

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+import VideoListPlay from './components/VideoListPlay';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [
+        { name:'视频1',sources:[{src:'https://www.runoob.com/try/demo_source/movie.mp4', type: 'video/mp4',}]},
+        { name:'视频2',sources:[{src:'https://www.w3school.com.cn/example/html5/mov_bbb.mp4', type: 'video/mp4',}]}
+      ],
+      visible: false
+    };
+  }
+  handleClick = () => {
+    this.setState({
+      visible: true
+    })
+  }
+  closeVideoPlay = () => {
+    this.setState({
+      visible: false
+    })
+  }
+  render() {
+    const { list, visible, closeVideoPlay } = this.state;
+    return (
+        <div>
+          <Button type="primary" onClick={this.handleClick}>视频弹框</Button>
+          {
+            visible &&
+            <VideoListPlay
+                videoList={list}
+                closeVideoPlay={this.closeVideoPlay}
+            />
+          }
+        </div>
+    );
+  }
 }
-
 export default App;
